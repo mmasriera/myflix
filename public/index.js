5a2850f738c459b7f440dd2ac490b311a95d53d1
Miquel Masriera <miquelmasriera@gmail.com>
2016-04-17 21:49:06 +0200

    react components

--- public/index.js
+++ public/index.js
@@ -1,12 +1,16 @@
 const React    = require( 'react' ),
-      ReactDOM = require( 'react-dom' );
+      ReactDOM = require( 'react-dom' ),
+      List     = require( './components/List' );
 
 const App = React.createClass({
 
     render() {
 
         return(
-            <div>it works</div>
+            <div>
+            	<h3>it works</h3>
+            	<List />
+        	</div>
         );
     }
 });
