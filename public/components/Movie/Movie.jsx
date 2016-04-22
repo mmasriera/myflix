e844ee58eb684157e8a26bd969d3a8c232b27537
Miquel Masriera <miquelmasriera@gmail.com>
2016-04-23 00:56:26 +0200

    webpack less

--- public/components/Movie/Movie.jsx
+++ public/components/Movie/Movie.jsx
@@ -3,11 +3,13 @@ const React = require( 'react' );
 module.exports = ( props ) => {
 
     return(
-        <div className="movie">
-            <h3>{ props.Title }</h3>
-            <p>{ props.Released }</p>
-            <p>{ props.Plot }</p>
-            {/*<img src={ props.Poster } />*/}
+        <div className="movie-container">
+            <div className="movie">
+                <h3>{ props.Title }</h3>
+                <p>{ props.Released }</p>
+                <p>{ props.Plot }</p>
+                {/*<img src={ props.Poster } />*/}
+            </div>
         </div>
     );
 }
