418233085b387807942187971b405d5ed1acb101
Miquel Masriera <miquelmasriera@gmail.com>
2016-05-17 21:30:42 +0200

    last update

--- public/index.jsx
+++ public/index.jsx
@@ -27,7 +27,7 @@ const App = React.createClass({
                 <div id="sidebar">
                     <div id="page-info">
                         <h2>{ "movies I've seen" }</h2>
-                        <p>last update +updated+</p>
+                        <p>last update LASTUPDATE</p>
                         <p>miquel masriera</p>
                         <hr></hr>
                     </div>
