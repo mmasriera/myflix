0b6eb5d6298ff6d9b75fbf8ac948d5ec36e341b8
Miquel Masriera <miquelmasriera@gmail.com>
2016-05-17 22:57:01 +0200

    update

--- prod.sh
+++ prod.sh
@@ -1,9 +1,5 @@
-
 # update bundle
 cp build/bundle.js bundle.js
-# set current date
-#currentdate=$(date +%d-%m-%Y)
-#sed -i '' "s/LASTUPDATE/$currentdate/" public/bundle.js
 # update posters
 rm -rf posters
 cp -r build/posters posters/
