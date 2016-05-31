428a3b619e2bf161b0f3b152884bf3abe67034bb
Miquel Masriera <miquelmasriera@gmail.com>
2016-05-31 18:17:12 +0200

    text

--- public/components/Movie/Movie.jsx
+++ public/components/Movie/Movie.jsx
@@ -8,4 +8,5 @@ module.exports = ( props ) =>
         <LazyLoad height={ 148 } offsetVertical={ 150 } >
             <img src={ `./posters/${ props.movie.Title }.jpg` } />
         </LazyLoad>
+        <p className="recommended">recommended</p>
     </div>;
