ba9a775ed78463d0e04198889965e39f3b46d521
Miquel Masriera <miquelmasriera@gmail.com>
2016-12-26 00:34:30 +0100

    loewr 1st letter

--- lists/listutils/addMovie.go
+++ lists/listutils/addMovie.go
@@ -22,12 +22,16 @@ func (mt MovieTitles) Len() int           { return len(mt) }
 func (mt MovieTitles) Swap(a, b int)      { mt[a], mt[b] = mt[b], mt[a] }
 func (mt MovieTitles) Less(a, b int) bool { return mt[a] < mt[b] }
 
+func lowerCaseFirst(s string) string {
+	return strings.ToLower(string(s[0])) + s[1:]
+}
+
 // AddMovie adds a movie to the list
 func AddMovie(newTitle string, file *os.File) {
 	movies := MovieTitles{newTitle}
 	input := bufio.NewScanner(file)
 	for input.Scan() { // line by line
-		title := strings.ToLower(input.Text())
+		title := lowerCaseFirst(input.Text())
 		if title != newTitle {
 			movies = append(movies, title)
 		}
